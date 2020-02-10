6eea1ebdc6d8fe9ae4ed997391be13f0a77ff197
Amanpreet Kaur <aman021011@gmail.com>
2020-02-09 22:08:40 -0500

    Initial Commit Layout

---
+++ app/src/main/java/com/example/android_766345aman/List0fFavtPlaces.java
@@ -0,0 +1,14 @@
+package com.example.android_766345aman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class List0fFavtPlaces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list0f_favt_places);
+    }
+}
